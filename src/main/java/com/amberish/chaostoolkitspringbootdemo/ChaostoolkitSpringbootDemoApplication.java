e9a2e8f4df4d6053b6c809c3713d9fdb321d897b
Amberish Raj <amberish.raj@gmail.com>
2021-08-05 19:46:57 +0530

    Initial commit - sprinboot project setup

---
+++ src/main/java/com/amberish/chaostoolkitspringbootdemo/ChaostoolkitSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.amberish.chaostoolkitspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaostoolkitSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaostoolkitSpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amberish/chaostoolkitspringbootdemo/ChaostoolkitSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amberish.chaostoolkitspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaostoolkitSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
